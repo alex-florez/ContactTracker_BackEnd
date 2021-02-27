c9ab89c84a35ad74b06338b4d5320366a6260cca
Alex Florez <UO264371@uniovi.es>
2021-02-27 14:21:39 +0100

    Pequeño cambio

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 const port = process.env.port || 8080
-const hostname = process.env.WEBSITE_HOSTNAME || 'localhost'
+// const hostname = process.env.WEBSITE_HOSTNAME || 'localhost'
+
+const hostname = 'localhost'
 
 app.get('/', (req, res) => {
     res.send("Hello World!! from Node.js Server")
