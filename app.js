6c26dd14b8e846b4c98d448141454a187c389bf7
Alex Florez <UO264371@uniovi.es>
2021-02-27 14:10:04 +0100

    Quitando hostname

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const port = process.env.port || 8080
 const hostname = 'localhost'
 
 app.get('/', (req, res) => {
-    res.send("Hello World!!!!")
+    res.send("Hello World!! from Node.js Server")
 })
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
     console.log(`Server listening at http://${hostname}:${port}`)
 })
