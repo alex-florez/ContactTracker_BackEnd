6480178b4175bb5268ca6fc947ec23a79f9d0bc4
Alex Florez <UO264371@uniovi.es>
2021-11-18 15:14:48 +0100

    Última revisión antes de la entrega

--- app.js
+++ app.js
@@ -2,20 +2,16 @@ const express = require('express')
 var cors = require('cors')
 require('dotenv').config({path: `.env.${process.env.NODE_ENV}`}) // Cargar variables de entorno
 
-const firebase = require('firebase-admin')
-
+const firebase = require('firebase-admin') // SDK de Firebase
 const app = express() // Aplicación Express
-
 const notificationManager = require('./di/AppModule.js').notificationManager() // Manager para las notificaciones.
 
 app.use(express.json()) // Body Parser para el JSON
 app.use(cors()) // CORS: para habilitar la cabecera Access-Control-Allow-Origin
 
-
 /* Router para la API REST */
 const {API} = require('./routes/api.js')
 app.use('/', API)
-
 
 /* Reprogramar Cron Tasks establecidas en la configuración */
 notificationManager.scheduleNotifications()
